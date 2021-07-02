8de7729a78e0cb56a034167d08ace6e6cbb9e417
Filiza <fk49201@ubt-uni.net>
2021-07-02 17:38:11 +0200

    Fitnes api version 1

---
+++ fitnesAPI/fitnesAPI/Controllers/TrainerController.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using fitnesAPI.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace fitnesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public TrainerController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        //Get Method
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @" 
+               select TrainerId, TrainerName, Fitnes, 
+               convert(varchar(10),DateOfJoining,120) as DateOfJoining 
+               ,PhotoFileName
+               from dbo.Trainer";
+
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TrainerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+        //post method
+        [HttpPost]
+        public JsonResult Post(Trainer tra)
+        {
+            string query = @" 
+               insert into dbo.Trainer 
+               (TrainerName, Fitnes,DateOfJoining, PhotoFileName)
+               values
+                 (
+                   '" + tra.TrainerName + @"'
+                   ,'" + tra.Fitnes + @"'
+                   ,'" + tra.DateOfJoining + @"'
+                   ,'" + tra.PhotoFileName + @"'
+                 )";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TrainerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+
+
+        [HttpPut]
+        public JsonResult Put(Trainer tra)
+        {
+            string query = @" 
+               update dbo.Trainer set
+                TrainerName = '" + tra.TrainerName + @"'
+               ,Fitnes = '" + tra.Fitnes + @"'
+               ,DateOfJoining = '" + tra.DateOfJoining + @"'
+               ,PhotoFileName = '" + tra.PhotoFileName + @"'
+              where TrainerId = " + tra.TrainerId + @"";
+
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TrainerAppCon");
+
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+        //we need to add it in the root parameter
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @" 
+               delete from dbo.Trainer 
+               where TrainerId = " + id + @"";
+
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TrainerAppCon");
+
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+
+        //API Method
+        [Route("SaveFile")]
+        [HttpPost]
+
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+                return new JsonResult("anonymous.png");
+            }
+        }
+
+        [Route("GetAllFitnesNames")]
+
+        public JsonResult GetAllFitnesNames()
+        {
+            string query = @" 
+               select FitnesName from dbo.Fitnes";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TrainerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+
+
+}
+
+
