59a4aaf410c2e6130e28f6d63e732a34eab1dc55
Filiza <fk49201@ubt-uni.net>
2021-07-02 17:56:24 +0200

    "Backend of fitness app"

---
+++ fitnesAPI/fitnesAPI/Models/Trainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fitnesAPI.Models
+{
+    public class Trainer
+    {
+        public int TrainerId { get; set; }
+        public string TrainerName { get; set; }
+        public string Fitnes { get; set; }
+
+        public string DateOfJoining { get; set;}
+
+        public string PhotoFileName { get; set; }
+
+    }
+}
